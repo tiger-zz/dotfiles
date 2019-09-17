47569ea068cd295b157f890a126346bcd2031a9d
Tiger Zhang <tzhang@cs.utexas.edu>
2019-09-17 01:30:02 -0500

    added nvim install script

--- install_nvim.sh
+++ install_nvim.sh
@@ -2,4 +2,8 @@
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim
+
 nvim +PlugInstall +qall
